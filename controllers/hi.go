c4e83e3574bafeff63ee16962771121153a17f78
Ropon <luopeng@codoon.com>
2021-12-21 11:42:33 +0800

    remove debug

--- controllers/hi.go
+++ controllers/hi.go
@@ -2,13 +2,9 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/ropon/logger"
 	"github.com/ropon/work_api/utils"
 )
 
 func Hi(c *gin.Context) {
-	logger.Debug("这是一条debug日志")
-	logger.Error("这是一条错误日志%s", c.ClientIP())
-	s := []string{"1","2"}
-	utils.GinOKRsp(c, s[2], "ok")
+	utils.GinOKRsp(c, "hi ropon!", "ok")
 }
