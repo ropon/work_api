7ecd0a0bb8870bd6966ec178d7fb7eb48342113b
Ropon <luopeng@codoon.com>
2023-06-07 16:40:04 +0800

    fix: new_project.sh

--- new_project.sh
+++ new_project.sh
@@ -8,7 +8,7 @@ pushd $(pwd)/$1
 rm -rf .git
 rm -f new_project.sh
 find . -type f|grep -vE "air$"|xargs -L 1 sed -i "" "s/work_api/$1/g"
-mv conf/ops_work_api.json conf/$1.json && cp conf/$1.json conf/${1}_dev.json
+mv conf/work_api.json conf/$1.json && cp conf/$1.json conf/${1}_dev.json
 find . -type f|grep -vE "air$"|xargs -L 1 sed -i "" "s/2345/$2/g"
 mkdir log
 git config --global init.defaultBranch master
